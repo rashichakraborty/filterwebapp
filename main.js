cec064b31e99d8fc633a589c6afa22ed36764e7e
rashichakraborty <77398317+rashichakraborty@users.noreply.github.com>
2022-03-01 18:50:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload(){
+  lipstick = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
+}
+ function setup(){
+     canvas = createCanvas(300,300);
+     canvas.center();
+     video = createCapture(VIDEO);
+     video.size(300,300);
+     video.hide();
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on("pose", gotPoses);
+ }
+
+ function draw(){
+   image(video,0,0,300,300);
+   fill(200,0,0);
+   stroke(255,0,0);
+   //circle(nose_x,nose_y,20);
+   image(lipstick,nose_x, nose_y, 40, 30);
+ }
+
+ function take_snapshot(){
+     save('Rashi.png');
+ }
+
+ function modelLoaded(){
+     console.log("PoseNet loaded");
+ }
+
+ function gotPoses(results){
+     if(results.length > 0){
+         console.log(results);
+         console.log("lips x  =" + results[0].pose.nose.x);
+         console.log("lips y  =" + results[0].pose.nose.y);
+         nose_x = results[0].pose.nose.x-20;
+         nose_y = results[0].pose.nose.y+20;
+     }
+ }
